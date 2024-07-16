e7a7ab983d1cd3caf9e1b865e12d0897ac591039
LAlinaV <ladik151104@gmail.com>
2024-07-16 09:28:24 +0300

    add variables and view components in onCreate

--- task2/app/src/main/java/com/example/task2/MainActivity.java
+++ task2/app/src/main/java/com/example/task2/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.task2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int num;
+    boolean isFinished;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvInfo = (TextView)findViewById(R.id.TextView);
+        etInput = (EditText)findViewById(R.id.editText);
+        bControl = (Button)findViewById(R.id.button);
+        num = (int)(Math.random()*100);
+        isFinished = false;
     }
 }
