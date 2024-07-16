436b17880166e50458c9ae71a09c0d2d8da00bd6
LAlinaV <ladik151104@gmail.com>
2024-07-16 09:30:56 +0300

    realise onClick

--- task2/app/src/main/java/com/example/task2/MainActivity.java
+++ task2/app/src/main/java/com/example/task2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.task2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,10 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvInfo = (TextView)findViewById(R.id.TextView);
-        etInput = (EditText)findViewById(R.id.editText);
-        bControl = (Button)findViewById(R.id.button);
+        tvInfo = findViewById(R.id.TextView);
+        etInput = findViewById(R.id.editText);
+        bControl = findViewById(R.id.button);
         num = (int)(Math.random()*100);
         isFinished = false;
+    }
+
+
+    public void onClick(View v) {
+        if (!isFinished) {
+            int inp = Integer.parseInt(etInput.getText().toString());
+            if (inp > num)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp < num)
+                tvInfo.setText(getResources().getString(R.string.behind));
+            if (inp == num) {
+                tvInfo.setText(getResources().getString(R.string.hit));
+                bControl.setText(getResources().getString(R.string.play_more));
+                isFinished = true;
+            }
+        } else {
+            num = (int) (Math.random() * 100);
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            isFinished = false;
+        }
+        etInput.setText("");
     }
 }
